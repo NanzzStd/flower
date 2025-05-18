ee0a9528ff7fcb6d49b51fa662df9a4a45771345
NanzzStd <samuraibattousai8@gmail.com>
2025-05-18 13:14:44 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Hi Kak Nadinda `.split('')
+const text = `Hi Kak Nadin `.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
